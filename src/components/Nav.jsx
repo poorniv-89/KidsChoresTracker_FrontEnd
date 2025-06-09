be1c8c0cae95cb949bd729400a5d5764794dd23f
poorniv-89 <poorniv.89@gmail.com>
2025-06-09 11:24:41 -0600

    Landing page completed

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,20 @@
 import { Link } from "react-router-dom";
+import ChoreBlasterzLogo from '../assets/ChoreBlasterzLogo.svg';
 
 export default function Nav() {
-    return (
-      <nav>
-        <ul>
+  return (
+    <nav className="homeNav">
+      <div className="navContainer">
+        <Link to="/">
+          <img src={ChoreBlasterzLogo} alt="ChoreBlasterz Logo" className="logo" />
+        </Link>
+        <ul className="navList">
           <li><Link to="/">Home</Link></li>
           <li><Link to="/how-it-works">How It Works</Link></li>
           <li><Link to="/about">About</Link></li>
           <li><Link to="/auth">Login</Link></li>
         </ul>
-      </nav>
-    );
-  }
+      </div>
+    </nav>
+  );
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,35 @@
 export default function HomePage(){
-    return <h1>HomePage</h1>
+    return (
+        <div>
+        <header>
+        <h1>Make Chores a Blast!</h1>
+        <p>
+          ChoreBlasterz helps families track chores, earn rewards and build responsibility —
+          the fun way!
+        </p>
+        <button>Get Started</button>
+        <div></div>
+      </header>
+      <section id="how-it-works">
+        <h2>How It Works</h2>
+        <ol>
+          <li>1. Parent assigns tasks</li>
+          <li>2. Kids complete them</li>
+          <li>3. Everyone earns rewards</li>
+        </ol>
+      </section>
+      <section>
+        <h2>Features</h2>
+        <ul>
+          <li>Multiple Kids & Parent Accounts</li>
+          <li>Badges & Levels</li>
+          <li>Point & Reward System</li>
+          <li>Daily/Weekly Chore Planner</li>
+        </ul>
+      </section>
+      <footer>
+        <button>Get Started</button>
+      </footer>
+    </div>
+    )
 }
