8e92383b04daf1fa3aa42214550478f2a49ad477
poorniv-89 <poorniv.89@gmail.com>
2025-06-12 11:07:09 -0600

    Added styling to addKid page

--- src/pages/AddKid.jsx
+++ src/pages/AddKid.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import axios from 'axios';
 import { useAuth } from '../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
+import '../styles/AddKidPage.css';
 
 export default function AddKidPage() {
     const { parentId } = useAuth();
@@ -24,18 +25,18 @@ export default function AddKidPage() {
     };
 
     return (
-        <div>
-            <h1>Add a New Kid</h1>
-            <form onSubmit={handleAddKid}>
-                <label>Kid's Name:</label>
-                <input
-                    type="text"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    required
-                />
-                <button type="submit">Add Kid</button>
-            </form>
+        <div className="add-kid-container">
+          <h1>Add a New Kid</h1>
+          <form onSubmit={handleAddKid}>
+            <label>Kid's Name:</label>
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+            <button type="submit">Add Kid</button>
+          </form>
         </div>
-    );
+      );
 }
