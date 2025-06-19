5f6c190586084aa3e41254a63a832997492b3e7b
poorniv-89 <poorniv.89@gmail.com>
2025-06-19 15:43:37 -0600

    Updated the base url

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: 'https://chore-backend-axp4.onrender.com/api',
 });
 
 API.interceptors.request.use((config) => {
